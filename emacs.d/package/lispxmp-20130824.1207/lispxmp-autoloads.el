2cb39dcbf2c5f789263e718f177179530e5b8ad0
natsuhiko.wake <natsuhiko.wake@GX1487.local>
2014-05-23 17:40:03 +0900

    update packages

---
+++ emacs.d/package/lispxmp-20130824.1207/lispxmp-autoloads.el
@@ -0,0 +1,18 @@
+;;; lispxmp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("lispxmp-pkg.el" "lispxmp.el") (21373
+;;;;;;  40553 287949 0))
+
+;;;***
+
+(provide 'lispxmp-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lispxmp-autoloads.el ends here
