a7b5ea704528f5d65715242fa1b13b8e6a1bc6ae
circularuins <circularuins.com>
2015-01-23 10:13:02 +0900

    24.4にしたらjaspaceでエラーが出たので、削除

--- emacs.d/conf/10-jaspace.el
+++
@@ -1,9 +0,0 @@
-;; jaspace.el を使った全角空白、タブ、改行表示モード
-;; 切り替えは M-x jaspace-mode-on or -off
-; ネットからインストールすると文字コードが変なので、手動でコピった
-; http://homepage3.nifty.com/satomii/software/jaspace.elayout.el
-(require 'jaspace)
-(setq jaspace-alternate-jaspace-string "□")
-;(setq jaspace-alternate-eol-string "↓\n")
-(setq jaspace-highlight-tabs t)  ; highlight tabs
-;(setq jaspace-highlight-tabs ?&gt;) ; use ^ as a tab marker
---
+++ emacs.d/conf/10-whitespace.el
@@ -0,0 +1,16 @@
+;; タブ, 全角スペース, 行末空白表示
+(defface my-face-b-1 '((t (:background "NavajoWhite4"))) nil) ; 全角スペース
+(defface my-face-b-2 '((t (:background "gray10"))) nil) ; タブ
+(defface my-face-u-1 '((t (:background "SteelBlue" :underline t))) nil) ; 行末空白
+(defvar my-face-b-1 'my-face-b-1)
+(defvar my-face-b-2 'my-face-b-2)
+(defvar my-face-u-1 'my-face-u-1)
+
+(defadvice font-lock-mode (before my-font-lock-mode ())
+  (font-lock-add-keywords
+   major-mode
+   '(("\t" 0 my-face-b-2 append)
+     ("　" 0 my-face-b-1 append)
+     ("[ \t]+$" 0 my-face-u-1 append))))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
