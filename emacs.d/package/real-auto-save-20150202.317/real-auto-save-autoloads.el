bea8b75766599e40e0e32c71cf9cb0b6065fa31d
circularuins <circularuins.com>
2015-02-03 09:59:27 +0900

    自動保存モードの設定

---
+++ emacs.d/package/real-auto-save-20150202.317/real-auto-save-autoloads.el
@@ -0,0 +1,16 @@
+;;; real-auto-save-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("real-auto-save.el") (21712 6497 209648
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; real-auto-save-autoloads.el ends here
---
+++ emacs.d/package/real-auto-save-20150202.317/real-auto-save-pkg.el
@@ -0,0 +1,1 @@
+(define-package "real-auto-save" "20150202.317" "Automatically save your all your buffers/files at regular intervals." 'nil)
