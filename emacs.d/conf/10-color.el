bae2768f5c5402a0d818e1a8e2b4afa34ba505a7
circularuins <circularuins.com>
2015-03-02 04:02:50 +0900

    カラーテーマの変更

--- emacs.d/conf/10-color.el
+++ emacs.d/conf/10-color.el
@@ -1,14 +1,11 @@
 ;; color-themeの設定
-; wget https://gnuemacscolorthemetest.googlecode.com/files/color-theme-6.6.0-mav.zip
-; unzipしたフォルダごと、elisp/に置く
-; GUIでは、自作テーマを仕様
+;; GUIでは自作テーマを仕様
+;; CUIではデフォルトテーマを使用
 (cond ((window-system)
        (setq custom-theme-directory "~/.emacs.d/themes/")
        (load-theme 'molokai t))
       (t
-       (require 'color-theme)
-       (color-theme-initialize)
-;       (color-theme-arjen)
-;       (color-theme-euphoria)
-       (color-theme-clarity)
+       (load-theme 'deeper-blue t)
+       (load-theme 'manoj-dark t)
+       (load-theme 'wheatgrass t)
        ))
