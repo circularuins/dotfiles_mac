2f3efe345f3e92e1b900a2be2f158fda0c6a28cc
natsuhikowake <wake@jmty.jp>
2015-06-11 14:49:38 +0900

    daily update packages

--- emacs.d/package/espuds-20140317.815/espuds-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; espuds-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("espuds-pkg.el" "espuds.el") (21356 20012
-;;;;;;  208300 0))
-
-;;;***
-
-(provide 'espuds-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; espuds-autoloads.el ends here
---
+++ emacs.d/package/espuds-20150604.1240/espuds-pkg.el
@@ -0,0 +1,1 @@
+(define-package "espuds" "20150604.1240" "Ecukes step definitions" '((s "1.7.0") (dash "2.2.0") (noflet "0.0.10") (f "0.12.1")) :url "http://github.com/ecukes/espuds" :keywords '("test"))
