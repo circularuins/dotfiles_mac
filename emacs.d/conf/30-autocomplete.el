bfdca42996486bc8a4d2a3c724de500f519deec1
natsuhikowake <wake@jmty.jp>
2015-06-11 15:42:07 +0900

    auto-complete関連の調整

--- emacs.d/conf/30-autocomplete.el
+++ emacs.d/conf/30-autocomplete.el
@@ -5,18 +5,23 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
 (ac-config-default)
 (setq ac-use-menu-map t) ;; C-n / C-p で選択
-(setq ac-auto-show-menu 0.3) ;; 候補が出るまでの時間 default 0.8
+(setq ac-use-fuzzy t) ;; 曖昧マッチ
+(setq ac-use-comphist t) ;; 補完推測機能有効
+;(setq ac-auto-show-menu 0.3) ;; 候補が出るまでの時間 default 0.8
 
 ;; ac-modeに各モードを追加する
 (add-to-list 'ac-modes 'lisp-mode)
-(add-to-list 'ac-modes 'slime-repl-mode)
+;(add-to-list 'ac-modes 'slime-repl-mode)
 (add-to-list 'ac-modes 'js2-mode)
 (add-to-list 'ac-modes 'scheme-mode)
-(add-to-list 'ac-modes 'inferior-scheme-mode)
-(add-to-list 'ac-modes 'inferior-lisp-mode)
+;(add-to-list 'ac-modes 'inferior-scheme-mode)
+;(add-to-list 'ac-modes 'inferior-lisp-mode)
 (add-to-list 'ac-modes 'clojure-mode)
 (add-to-list 'ac-modes 'yaml-mode)
-(add-to-list 'ac-modes 'eshell-mode)
+;(add-to-list 'ac-modes 'eshell-mode)
+(add-to-list 'ac-modes 'text-mode)
+(add-to-list 'ac-modes 'fundamental-mode)
+(add-to-list 'ac-modes 'org-mode)
 
 ;; ac-mode
 ;(autoload 'ac-mode "ac-mode" "Minor mode for advanced completion." t nil)
