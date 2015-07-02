07d4ac452daca95ea86a2a017a85ffa96ef53039
natsuhikowake <wake@jmty.jp>
2015-07-02 16:25:30 +0900

    daily update packages

--- emacs.d/package/espuds-20150604.1240/espuds-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; espuds-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("espuds.el") (21881 8488 74271 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; espuds-autoloads.el ends here
