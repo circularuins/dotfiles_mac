111eb0911f09dc8fef0534e5e8ce6271e3aace46
natsuhikowake <wake@jmty.jp>
2015-07-07 14:45:49 +0900

    カラーテーマをいい感じのやつに変更

--- emacs.d/conf/10-color.el
+++ emacs.d/conf/10-color.el
@@ -1,11 +1,16 @@
 ;; color-themeの設定
 ;; GUIでは自作テーマを仕様
 ;; CUIではデフォルトテーマを使用
-(cond ((window-system)
-       (setq custom-theme-directory "~/.emacs.d/themes/")
-       (load-theme 'molokai t))
-      (t
-       (load-theme 'deeper-blue t)
-       (load-theme 'manoj-dark t)
-       (load-theme 'wheatgrass t)
-       ))
+;; (cond ((window-system)
+;;        (setq custom-theme-directory "~/.emacs.d/themes/")
+;;        (load-theme 'molokai t))
+;;       (t
+;;        (load-theme 'deeper-blue t)
+;;        (load-theme 'manoj-dark t)
+;;        (load-theme 'wheatgrass t)
+;;        ))
+
+;; (load-theme 'tronesque t)
+;; (tronesque-mode-line)
+
+(load-theme 'niflheim t)
