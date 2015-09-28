76e7949037cd03e01e60999b2ca83f6fd3d35c4f
wake <wake@jmtyadministrator-no-MacBook-Air.local>
2015-09-28 12:21:25 +0900

    update cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-pprint "1.1.2"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
