b01429c6071f33d3485f1239c7c2d8a64625aa18
wake <wake@jmtyadministrator-no-MacBook-Air.local>
2015-11-16 11:15:51 +0900

    daily update packages

---
+++ emacs.d/package/espuds-20151114.759/espuds-autoloads.el
@@ -0,0 +1,15 @@
+;;; espuds-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("espuds.el") (22089 15347 412952 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; espuds-autoloads.el ends here
--- emacs.d/package/web-mode-20151107.26/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20151107.26" "major mode for editing web templates" 'nil :url "http://web-mode.org" :keywords '("languages"))
