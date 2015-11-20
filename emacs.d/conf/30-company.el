003424284b66c2dabd562580634c3331ec55d7ce
wake <wake@jmtyadministrator-no-MacBook-Air.local>
2015-11-20 13:08:55 +0900

    company-modeの設定修正

--- emacs.d/conf/30-company.el
+++ emacs.d/conf/30-company.el
@@ -1,3 +1,12 @@
-(global-company-mode 1)
+(global-company-mode)
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
 
 (add-to-list 'company-backends 'company-web-html)
+
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-h") nil)
