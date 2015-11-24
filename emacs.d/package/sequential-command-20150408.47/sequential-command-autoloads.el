7050d23de281e5f62d2b35e5a6fb16c08b5ea8f5
wake <wake@jmtyadministrator-no-MacBook-Air.local>
2015-11-24 12:09:31 +0900

    daily update packages

--- emacs.d/package/sequential-command-20150408.47/sequential-command-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; sequential-command-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "sequential-command-config" "sequential-command-config.el"
-;;;;;;  (21797 1693 0 0))
-;;; Generated autoloads from sequential-command-config.el
-
-(autoload 'sequential-command-setup-keys "sequential-command-config" "\
-Rebind C-a, C-e, M-u, M-c, and M-l to seq-* commands.
-If you use `org-mode', rebind C-a and C-e.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("sequential-command-pkg.el" "sequential-command.el")
-;;;;;;  (21797 1693 485841 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sequential-command-autoloads.el ends here
---
+++ emacs.d/package/sequential-command-20151116.1228/sequential-command-autoloads.el
@@ -0,0 +1,28 @@
+;;; sequential-command-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "sequential-command-config" "sequential-command-config.el"
+;;;;;;  (22099 54418 0 0))
+;;; Generated autoloads from sequential-command-config.el
+
+(autoload 'sequential-command-setup-keys "sequential-command-config" "\
+Rebind C-a, C-e, M-u, M-c, and M-l to seq-* commands.
+If you use `org-mode', rebind C-a and C-e.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("sequential-command-pkg.el" "sequential-command.el")
+;;;;;;  (22099 54418 7837 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sequential-command-autoloads.el ends here
