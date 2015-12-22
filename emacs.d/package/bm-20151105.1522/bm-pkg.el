3314c80903db8d0c928c32e423011f80c7b7cb32
wake <wake@jmtyadministrator-no-MacBook-Air.local>
2015-12-22 10:55:16 +0900

    daily update packages

--- emacs.d/package/bm-20151105.1522/bm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bm" "20151105.1522" "Visible bookmarks in buffer." 'nil :url "https://github.com/joodland/bm" :keywords '("bookmark" "highlight" "faces" "persistent"))
---
+++ emacs.d/package/bm-20151221.115/bm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bm" "20151221.115" "Visible bookmarks in buffer." 'nil :url "https://github.com/joodland/bm" :keywords '("bookmark" "highlight" "faces" "persistent"))
