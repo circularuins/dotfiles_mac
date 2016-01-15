300e742077745bf8e86f34b038f769186dae7894
wake <wake@jmtyadministrator-no-MacBook-Air.local>
2016-01-15 21:58:22 +0900

    bugfix

--- emacs.d/conf/40-js.el
+++ emacs.d/conf/40-js.el
@@ -9,3 +9,4 @@
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
 (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
+(add-hook 'js2-mode-hook (lambda () (company-mode -1)))
