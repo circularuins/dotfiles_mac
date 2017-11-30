573b16d25b9b90a3766ed9945affe3509ca6bba2
circularuins <natsuhiko.wake@gmail.com>
2017-11-30 23:09:17 +0900

    cider-nreplの設定を更新

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-pprint "1.1.2"]
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.15.1"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
